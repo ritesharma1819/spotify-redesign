b30fb79dabf3120c8cd3c833b2b8ce5247a76334
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-03-18 00:16:27 +0530

    added logo  for our local spotify-redesign

---
+++ src/Leftbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Leftbar.scss"
+
+const Leftbar=()=>{
+    return(
+        <div className='container'>
+             <img className='logo' src={require('./Spotify-redesign -logos.jpeg')} alt='logo' />
+
+        </div>
+    );
+}
+
+export default Leftbar;
