9ed674721511e4f03434814b9fa4e00cb3a31bfb
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-03-18 00:05:32 +0530

    create leftbar file for making left side U.I

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./App.scss";
+import Leftbar from "./Leftbar"
 
 const App =()=>{
     return(
         <div>
-
+           <Leftbar />
         </div>
     );
 }
