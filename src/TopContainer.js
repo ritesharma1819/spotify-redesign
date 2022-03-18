e32de2a65ef212f7f9efe277d7fa0e3dff50a56b
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-03-18 15:30:13 +0530

    added styling and icon for top container in leftbar

--- src/TopContainer.js
+++ src/TopContainer.js
@@ -3,11 +3,25 @@ import { FaHome } from 'react-icons/fa';
 import "./TopContainer.scss";
 
 
+
 const TopContainer =()=>{
     return(
-        <div className='home_icon'>
-            <FaHome size={30} />
-            <p className='home_heading'>Home</p>
+        <div className='topcontainer_icon'>
+            <div className='home_icon'>
+                <FaHome size={25} />
+                <p className='home_heading'>Home</p>
+            </div>
+
+            <div className='trends_icon'>
+                <FaHome size={25} />
+                <p className='trends_heading'>Trends</p>
+            </div>
+
+            <div className='library_icon'>
+                <FaHome size={25} />
+                <p className='library_heading'>Library</p>
+            </div> 
+            
         </div>
     );
 }
